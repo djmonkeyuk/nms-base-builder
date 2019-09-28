9fb09de7f81c53fb4ca11ee5c13f938fdb705e9e
Charlie Banks <charliembbanks@gmail.com>
2019-09-28 12:04:54 +0100

    Fixed version number of plugin

--- src/no_mans_sky_base_builder/__init__.py
+++ src/no_mans_sky_base_builder/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "name": "No Mans Sky Base Builder",
     "description": "A tool to assist with base building in No Mans Sky",
     "author": "Charlie Banks",
-    "version": (1, 1, 0),
+    "version": (1, 1, 2),
     "blender": (2, 80, 0),
     "location": "3D View > Tools",
     "warning": "",  # used for warning icon and text in addons panel

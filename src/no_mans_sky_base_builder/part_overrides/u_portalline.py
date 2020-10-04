6ebf61d52fff5200042ac9a5bce605d63c407982
Ben Jackson <ben@ben.com>
2020-10-04 10:27:06 -0700

    Make U_PORTALLINE material teal

--- src/no_mans_sky_base_builder/part_overrides/u_portalline.py
+++ src/no_mans_sky_base_builder/part_overrides/u_portalline.py
@@ -1,6 +1,8 @@
 import no_mans_sky_base_builder.part_overrides.line as line
+import no_mans_sky_base_builder.utils.material as material
 
 
 class U_PORTALLINE(line.Line):
     def __init__(self, *args, **kwargs):
         super(U_PORTALLINE, self).__init__(*args, **kwargs)
+        material.assign_portal_material(self.object)

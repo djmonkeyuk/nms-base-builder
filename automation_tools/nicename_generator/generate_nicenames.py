4a17681e2618994e82b08cc873de02a724d3c1e8
Charlie Banks <charliembbanks@gmail.com>
2022-11-13 22:34:37 +0000

    Added nicename generation tool. Updated asset_data.yaml file.

---
+++ automation_tools/nicename_generator/generate_nicenames.py
@@ -0,0 +1,55 @@
+import json
+import os
+import sys
+import xml.etree.ElementTree as ET
+from pprint import pprint
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+
+from automation_utils import PATH_TO_MOD_PROJECT, get_all_existing_parts
+
+all_parts = get_all_existing_parts()
+
+
+LOC1_PATH = "LANGUAGE/NMS_LOC1_ENGLISH.EXML"
+LOC4_PATH = "LANGUAGE/NMS_LOC4_ENGLISH.EXML"
+LOC5_PATH = "LANGUAGE/NMS_LOC5_ENGLISH.EXML"
+LOC6_PATH = "LANGUAGE/NMS_LOC6_ENGLISH.EXML"
+LOC7_PATH = "LANGUAGE/NMS_LOC7_ENGLISH.EXML"
+UPDATE3_PATH = "LANGUAGE/NMS_UPDATE3_ENGLISH.EXML"
+PRODUCT_PATH = "METADATA/REALITY/TABLES/NMS_REALITY_GCPRODUCTTABLE.EXML"
+PRODUCT_PATH = os.path.join(PATH_TO_MOD_PROJECT, PRODUCT_PATH)
+LOC1_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, LOC1_PATH)
+LOC4_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, LOC4_PATH)
+LOC5_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, LOC5_PATH)
+LOC6_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, LOC6_PATH)
+LOC7_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, LOC7_PATH)
+UPDATE3_FULL_PATH = os.path.join(PATH_TO_MOD_PROJECT, UPDATE3_PATH)
+EXPORT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "export", "nice_names.json")
+
+
+
+# Build language reference.
+data = {}
+for path in [LOC1_FULL_PATH, LOC4_FULL_PATH, LOC5_FULL_PATH, LOC6_FULL_PATH, LOC7_FULL_PATH, UPDATE3_FULL_PATH]:
+    language_tree = ET.parse(path)
+    language_root = language_tree.getroot()
+    local_table = language_root[0]
+    for table in local_table:
+        id = table[0].attrib["value"]
+        data[id] = table[1][0].attrib["value"]
+
+# Build
+product_tree = ET.parse(PRODUCT_PATH)
+product_root = product_tree.getroot()
+table_root = product_root[0]
+language_ref = {}
+for child in table_root:
+    id = child[0].attrib["value"]
+    name_ref = child[1].attrib["value"]
+    if (child[14][0].attrib["value"] == "BuildingPart") and (name_ref in data):
+        language_ref[id] = data[name_ref]
+
+
+with open(EXPORT_PATH, "w") as stream:
+    json.dump(language_ref, stream, indent=4)
--- src/addons/no_mans_sky_base_builder/asset_browser/utils.py
+++ src/addons/no_mans_sky_base_builder/asset_browser/utils.py
@@ -75,15 +75,6 @@ def import_folder_into_maya():
         mesh = [x for x in post if x not in pre]
         cmds.rename(mesh[0], id)
 
-def list_missing_icons():
-    unpacked_path = "N:/Games/No Mans Sky/nms_modding_station/unpacked/TEXTURES/UI/FRONTEND/ICONS/BUILDABLE"
-    return [x.split(".")[1]+".PNG" for x in os.listdir(unpacked_path)]
-    # from Image import image
-    # with image.Image(filename="white_rect_dxt3.dds") as img:
-    #     img.compression = "no"
-    #     img.save(filename="white_rect_dxt3.png")
-
-
 def write_qrc_contents():
     folder_dir = os.path.dirname(os.path.realpath(__file__))
     icons_dir = os.path.join(folder_dir, "icons")
@@ -126,4 +117,6 @@ def remove_unused_icons():
             pass
 
 
-print(list_missing_icons())
+parts = list_missing_parts()
+for part in parts:
+    print ("- {}".format(part))

6187c6a0ce5c5079523a35cecb1f621f4b557b5b
Charlie Banks <charliembbanks@gmail.com>
2022-11-14 10:32:33 +0000

    Added nice names to DT_PartDefinition

--- automation_tools/DT_generator/generate_partdefinition.py
+++ automation_tools/DT_generator/generate_partdefinition.py
@@ -1,17 +1,23 @@
 import csv
+import json
 import os
 import sys
 from copy import deepcopy
 from pprint import pprint
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+sys.path.append(root_dir)
+
+NICE_NAME_FILE = os.path.join(root_dir, "nicename_generator", "export", "nice_names.json")
+with open(NICE_NAME_FILE, "r") as stream:
+    nice_name_data = json.load(stream)
 
 from automation_utils import get_all_existing_parts
 
 EXISTING_CSV_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "DT_PartDefinition.csv")
 # ---,ObjectModel,Category,Icon,SubCategory,SocketClassIDs,PlugClassIDs
 
-headings = ["---", "ObjectModel", "Category", "Icon", "SubCategory", "SocketClassIDs", "PlugClassIDs"]
+headings = ["---", "ObjectModel", "Category", "Icon", "SubCategory", "SocketClassIDs", "PlugClassIDs", "NiceName"]
 
 existing_rows = {}
 with open(EXISTING_CSV_FILE) as csv_file:
@@ -37,12 +43,15 @@ for key, value in existing_rows.items():
     nice_key = key.replace("^", "")
     value[1] = UE_model_path+nice_key+"."+nice_key+"'"
     value[3] = UE_texture_path+nice_key+"."+nice_key+"'"
+    value[7] = nice_name_data.get(nice_key, "")
 
 build_data = ""
-build_data += "---,ObjectModel,Category,Icon,SubCategory,SocketClassIDs,PlugClassIDs\n"
-for key, value in existing_rows.items():
-    build_data += "{},\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
-        value[0], value[1], value[2], value[3], value[4], value[5].replace("\"", "\"\""), value[6].replace("\"", "\"\"")
+build_data += "---,ObjectModel,Category,Icon,SubCategory,SocketClassIDs,PlugClassIDs,NiceName\n"
+for key in sorted(existing_rows.keys()):
+    value = existing_rows[key]
+    build_data += "{},\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
+        value[0], value[1], value[2], value[3], value[4], value[5].replace("\"", "\"\""), value[6].replace("\"", "\"\""), value[7]
     )
+
 with open(EXISTING_CSV_FILE, mode='w', newline='', encoding='utf-8') as csv_file:
     csv_file.write(build_data)

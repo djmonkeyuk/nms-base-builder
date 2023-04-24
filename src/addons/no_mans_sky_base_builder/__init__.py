1ab0d2a3a61aec1dceb4da91fff2fba7ffb2e5a2
Charlie Banks <charliembbanks@gmail.com>
2023-04-24 21:39:53 +0100

    Update to 2.0.5

--- src/addons/no_mans_sky_base_builder/__init__.py
+++ src/addons/no_mans_sky_base_builder/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "name": "No Mans Sky Base Builder",
     "description": "A tool to assist with base building in No Mans Sky",
     "author": "Charlie Banks",
-    "version": (2, 0, 4),
+    "version": (2, 0, 5),
     "blender": (3, 0, 0),
     "location": "3D View > Tools",
     "warning": "",  # used for warning icon and text in addons panel

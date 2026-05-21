ab10a35ab5c198268497ce39982e54ad0fd10ced
Kuma the Wizard <277236726+kuma-the-wizard@users.noreply.github.com>
2026-05-21 09:05:10 +0530

    Added a module to help with translation of JSON data, along with dictionary of necessary keys.

---
+++ src/addons/no_mans_sky_base_builder/save_editor/save_translation.py
@@ -0,0 +1,53 @@
+import os
+from ..utils import python as python_utils
+
+# load save map that contains collection of only used keys
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+SAVE_MAP_JSON_FILE = os.path.join(FILE_PATH, "save_map_dictionary.json")
+
+#store dictionary in reversed and original order for efficient searching
+save_map_dictionary_reversed = python_utils.load_dictionary(SAVE_MAP_JSON_FILE)
+save_map_dictionary = {v: k for k, v in save_map_dictionary_reversed.items()}
+
+#collection of most used keys
+class SaveTranslation:
+    base_context = "vLc"
+    player_state_data = "6f="
+    persistent_player_bases = "F?0"
+    base_name = "NKm"
+    base_type = "peI"
+    persistent_base_types = "DPp"
+    galactic_address = "oZw"
+    save_name = "Pk4"
+    user_data = "CVX"
+    objects = "@ZJ"
+    
+    
+# this function recursively replaces each key in object with respect to mapping provided by translator
+def translate_data(obj,translator):
+        if isinstance(obj, dict):
+            new_dict = {} 
+            for key, value in obj.items():
+                # Translate the key if it exists in the map, otherwise keep the original
+                new_key = translator(key)
+                # Recursively process the value (to handle nested dicts/lists)
+                new_dict[new_key] = translate_data(value, translator)
+                
+            return new_dict
+        elif isinstance(obj, list):
+            # if child is a list, process every item in it
+            return [translate_data(item, translator) for item in obj]
+        else:
+            return obj
+
+# helpers to map keys
+eng_to_obf_translator = lambda k: save_map_dictionary.get(k, k)
+obf_to_eng_translator = lambda k: save_map_dictionary_reversed.get(k, k)
+
+#  translate to english data
+def translate_to_eng_data(data):
+    return translate_data(data,obf_to_eng_translator)
+
+# translate to obfuscated data
+def translate_to_obf_data(data):
+    return translate_data(data,eng_to_obf_translator)
